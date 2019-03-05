8bc48912435a143f06296cded60136520a2bb5a3
Dan Kagan <dankagan7@gmail.com>
2019-03-05 23:25:18 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,28 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  var obj = {prop: 1};
+    Object.assign(obj, {"prop2": 2});
+  return obj;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  obj[key] = value;
+      return obj;
+}
+
+function deleteFromObjectByKey(object, key) {
+    var obj = { prop: 1 };
+    
+    delete obj.prop;
+    return obj;
+}
+
+function destructivelyDeleteFromObjectByKey(obj, key) {
+  var newObj = Object.assign({}, obj);
+
+    delete obj.prop;
+      return obj;
+}
+
+
